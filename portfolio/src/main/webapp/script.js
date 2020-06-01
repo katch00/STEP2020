898025abf906b7906949434f4bc9bb1d8a57221a
Nat <nstroupe0215@gmail.com>
2020-06-01 15:09:46 -0500

    step2 walkthrough content

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -40,4 +40,8 @@ function randomPik() {
   imageContainer.appendChild(imgElement);
 }
 
-
+function getMessage() {
+  fetch('/data').then(response => response.text()).then((message) => {
+    document.getElementById('message-container').innerText = message;
+  });
+}
